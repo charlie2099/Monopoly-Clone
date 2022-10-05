58f2ba0c44daa497d9d70af752ebeea463f0dcd3
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-10-05 22:30:15 +0100

    Added all colour properties

---
+++ Monopoly Clone/Assets/Scripts/ScriptableObjects/TileType.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "TileTypeData", menuName = "ScriptableObjects/TileTypeData")]
+    public class TileType : ScriptableObject
+    {
+        public string name;
+        public Texture imageTexture;
+        public GameObject reward;
+        
+        /*
+         * Types of tiles
+         * - Properties with houses (colour, stackable)
+         * - Properties without houses (stations etc)
+         * - Special (community chest, chance)
+         * - GO, Jail, Freeparking, Go To Jail
+         */
+    }
+}
