9a08795ccafa253b6534b9e4997bcc042df104b5
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-10-06 00:09:13 +0100

    Laid out all tiles on board

--- Monopoly Clone/Assets/Scripts/ScriptableObjects/TileType.cs
+++ Monopoly Clone/Assets/Scripts/ScriptableObjects/TileType.cs
@@ -15,6 +15,18 @@ namespace ScriptableObjects
          * - Properties without houses (stations etc)
          * - Special (community chest, chance)
          * - GO, Jail, Freeparking, Go To Jail
+         *
+         *
+         * Property Block Tile (name, image, cost, colour, houses)
+         * Property Tile (name, image, cost)
+         * Special Tile (name, image, pick up cards)
+         * Tax Tile (name, image, cost)
+         * Corner Tile (name, image)
+         * GO (name, image, pick up money)
+         *
+         * Properties In Common
+         * - Name
+         * - Image
          */
     }
 }
