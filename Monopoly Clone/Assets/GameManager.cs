c11d225fb3e956517c2bf666aaee03cd9f1b65d5
Charlie <charlie6.evans@live.uwe.ac.uk>
2022-10-07 03:09:38 +0100

    Added simple turn based system

---
+++ Monopoly Clone/Assets/GameManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tiles;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+    public Player ActivePlayer() => _currentPlayer;
+    public List<Tile> Tiles => tiles;
+    
+    [SerializeField] private List<Player> players;
+    [SerializeField] private List<Tile> tiles;
+    private Player _currentPlayer;
+    private int _turnIndex;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        _currentPlayer = players[0];
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextTurn();
+        }
+    }
+
+    public void NextTurn()
+    {
+        _turnIndex++;
+        if (_turnIndex >= players.Count)
+        {
+            _turnIndex = 0;
+        }
+        _currentPlayer = players[_turnIndex];
+
+    }
+}
---
+++ Monopoly Clone/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Commands;
+using Tiles;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Piece piece;
+    private readonly CommandInvoker _commandInvoker = new();
+
+    private void Update()
+    {
+        if (GameManager.Instance.ActivePlayer() == this)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                _commandInvoker.AddCommand(new MoveCommand(piece, piece.CurrentTile(), GameManager.Instance.Tiles[Random.Range(0, GameManager.Instance.Tiles.Count)]));
+            }
+        }
+    }
+}
